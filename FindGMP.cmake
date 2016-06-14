37bdb52e156fdadddce051cbb79bb9638455b952
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-06-14 14:37:13 +0200

    Remove platform specific code from GMP library finder.

--- FindGMP.cmake
+++ FindGMP.cmake
@@ -7,16 +7,15 @@
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-	
+
 if (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
   # Already in cache, be silent
   set(GMP_FIND_QUIETLY TRUE)
 endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
 
-find_path(GMP_INCLUDE_DIR NAMES gmp.h PATHS /export/scratch1/home/as/miniconda2/envs/new-dream/include)
-find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS /export/scratch1/home/as/miniconda2/envs/new-dream/lib)
-find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
-MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
+find_path(GMP_INCLUDE_DIR NAMES gmp.h )
+find_library(GMP_LIBRARIES NAMES gmp libgmp )
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx )
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
